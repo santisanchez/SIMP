03c1a93ad5cf1ebcec7ec35c5c4a3c1b67e20ba6
Santiago Sanchez <santisanchez.1214@gmail.com>
2019-09-18 22:04:10 -0500

    Added migration to create bills table

---
+++ database/migrations/2019_09_15_180550_create_bill_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBillTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('price')->unsigned();
+            $table->integer('store_id')->unsigned();
+            $table->foreign('store_id')->references('id')->on('stores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bill');
+    }
+}
